46c6c6f291f6a346af3486fe521b90dd52e387af
ForgeDom <maxstill2006@gmail.com>
2024-05-02 18:26:42 +0300

    Lab ex2 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int a, b;
+	
+	cin >> a >> b;
+	while (a <= b) {
+		cout << a << endl;
+		++a;
+	}
 	return 0;
 }
