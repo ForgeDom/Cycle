3e6177a71e6202e4997907dc0deca571743086e2
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:23:26 +0300

    hm ex3 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int sum = 0;
+	int next = 0;
+
+	while (next < 1000) {
+		sum += next + 1;
+		next++;
+	}
+	double average = static_cast<double>(sum) / next;
+	cout << average<< endl;
 	return 0;
 }
