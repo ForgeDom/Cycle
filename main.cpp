3c7377c5a1970615c19c2007965a331510d14ca9
ForgeDom <maxstill2006@gmail.com>
2024-05-02 18:21:29 +0300

    Lab ex1 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
+	
+	int a;
+	
+	cin >> a;
+	for (int i = 0; i <= a; ++i) {
+		cout << i << endl;
+	}
+	
 	return 0;
 }
