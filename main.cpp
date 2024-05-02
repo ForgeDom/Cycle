d134e4a46885748a73d679af40b8981b9bd8ab0c
ForgeDom <maxstill2006@gmail.com>
2024-05-02 20:15:54 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+
+	int L, P;
+	cin >> L >> P;
+	int pillow = 0;
+	while (L >= P) {
+		L -= P;
+		pillow++;
+	}
+	cout << pillow << endl;
 
 	return 0;
 }
