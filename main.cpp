81ce8f1b7028d6c7a2ef60114cd41b0078d83533
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:50:40 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int a , choise;
+	char b;
+	cout << "Введіть кількість символів: " << endl;
+	cin >> a;
+
+	cout << "Введіть тип символу: " << endl;
+	cin >> b;
+
+	cout << "Горизонтальна(1)/Вертикальна(2)?" << endl;
+	cin >> choise;
+
+	switch (choise) {
+		case 1:
+			while (a > 0) {
+				cout << b << "";
+				--a;
+			}
+			cout << endl;
+			break;
+		case 2:
+			while (a > 0) {
+				cout << b << endl;
+				--a;
+			}
+			break;
+	}	
 	return 0;
 }
