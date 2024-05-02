50b89502cf3eee3f1be6ec713b71eba79ad789a3
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:05:20 +0300

    hm ex1 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int a;
+	int sum = 0;
+
+	cin >> a;
+	if (a <= 500) {
+		while (a <= 500) {
+			sum += a;
+			a++;
+		}
+	}
+	cout << sum << endl;
 	return 0;
 }
