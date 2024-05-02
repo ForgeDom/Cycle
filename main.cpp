7b84cf477ec0b9c4a0c0fdcf1fe7f6269c7a603f
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:34:56 +0300

    hm ex5 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int k;
+	int i = 1;
+	
+	cin >> k;
+	while (i <= 10) {
+		cout << k << "*" << i << "=" << k * i << endl;
+		++i;
+	}
 	return 0;
 }
