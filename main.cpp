4817182f08244aba713cb064eca084b5113e2c88
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:27:34 +0300

    hm ex4 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int a;
+	int sum = 0;
+	cin >> a;
+	
+	do {
+		sum += a;
+		++a;
+	} while (1 <= a && a <= 20);
+	cout << sum << endl;
 	return 0;
 }
