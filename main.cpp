cf363da2d2a72aaf8a01bc62e9c48dd0414e7230
ForgeDom <maxstill2006@gmail.com>
2024-05-02 18:56:27 +0300

    lab ex3 done

--- main.cpp
+++ main.cpp
@@ -6,5 +6,14 @@ int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int c, d;
+	int sum = 0;
+
+	cin >> c >> d;
+	do {
+		sum += c;
+		c++;
+	} while (c<=d);
+	cout << sum << endl;
 	return 0;
 }
