19c8f21bdc2026f43ad93faf5079f6cce9ba9e0c
ForgeDom <maxstill2006@gmail.com>
2024-05-02 20:08:29 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int n;
+	int i = 1;
+	int fact = 1;
+	cin >> n;
+
+	if (n < 0) {
+		cout << "Error" << endl;
+	}
+	else {
+		while (i <= n) {
+			fact *= i;
+			++i;
+		}
+		cout << fact << endl;
+	}
 	return 0;
 }
