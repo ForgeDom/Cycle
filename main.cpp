62e95c01e02ad1971869d1f5a997ecc7e5f88384
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:00:17 +0300

    lab ex4 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int n;
+	int sum = 0;
+
+	cin >> n;
+
+	while (n != 0) {
+		cin >> n;
+		sum += n;
+	}
+	cout << sum << endl;
 	return 0;
 }
