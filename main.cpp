6e26bad51939e97340459dd477c78c79f96c4f49
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:58:34 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int a, b;
+	cin >> a >> b;
+	int sum = 0;
+
+	while (a <= b) {
+		if (a % 2 != 0) {
+			sum += a;
+			
+		}
+		++a;
+	}
+	cout << sum << endl;
 	return 0;
 }
