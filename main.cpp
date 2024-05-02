a260335380a69a1fbba63468f29b6e4bb95511f5
ForgeDom <maxstill2006@gmail.com>
2024-05-02 19:14:00 +0300

    hm ex2 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int x, y;
+	int begin = 1;
+	int i = 0;
+
+	cin >> x >> y;
+
+	while (i < y) {
+		begin *= x;
+		++i;
+	}
+	cout << begin << endl;
 	return 0;
 }
