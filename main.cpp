3ef5ad8602e7d196a60fc5f62fef8ea1871e1e5d
ForgeDom <maxstill2006@gmail.com>
2024-05-02 20:22:40 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <Windows.h>
+using namespace std;
 
 int main() {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
+	int a;
+	cout << "Скільки ящиків на складі?" << endl;
+	cin >> a;
+	int b;
+	cout << "Скільки ящиків вміщується в 1 машину?" << endl;
+	cin >> b;
+	int cars = 0;
+	while (a != 0) {
+		a -= b;
+		cars++;
+	}
+	cout << "Необхідно" << cars << "машин!" << endl;
 	return 0;
 }
